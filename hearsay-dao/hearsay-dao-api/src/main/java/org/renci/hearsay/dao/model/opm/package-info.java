7e4819d1285cbe85035bee853a2f221e5ba782ec
Jason Reilly <jdr0887@renci.org>
2014-04-29 10:37:10 -0400

    removing

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/model/opm/package-info.java
+++
@@ -1,6 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.w3.org/ns/prov#", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED, xmlns = {
-        @javax.xml.bind.annotation.XmlNs(prefix = "prov", namespaceURI = "http://www.w3.org/ns/prov#"),
-        @javax.xml.bind.annotation.XmlNs(prefix = "xsd", namespaceURI = "http://www.w3.org/2001/XMLSchema") })
-@javax.xml.bind.annotation.adapters.XmlJavaTypeAdapters({ @javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter(IDRefAdapter.class) })
-package org.renci.hearsay.dao.model.opm;
-
