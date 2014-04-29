fd6a8702f7c00056aa80c131e0d26235ba256bf5
Jason Reilly <jdr0887@renci.org>
2014-04-29 10:37:18 -0400

    removing

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/opm/ActivityDAO.java
+++
@@ -1,8 +0,0 @@
-package org.renci.hearsay.dao.opm;
-
-import org.renci.hearsay.dao.BaseDAO;
-import org.renci.hearsay.dao.model.opm.Activity;
-
-public interface ActivityDAO extends BaseDAO<Activity, Long> {
-
-}
