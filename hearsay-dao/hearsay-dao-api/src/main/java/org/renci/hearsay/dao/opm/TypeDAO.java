0a947949acd8f4f2d053a211a47f386a9b5018fd
Jason Reilly <jdr0887@renci.org>
2014-04-29 10:37:18 -0400

    removing

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/opm/TypeDAO.java
+++
@@ -1,8 +0,0 @@
-package org.renci.hearsay.dao.opm;
-
-import org.renci.hearsay.dao.BaseDAO;
-import org.renci.hearsay.dao.model.opm.Type;
-
-public interface TypeDAO extends BaseDAO<Type, Long> {
-
-}
