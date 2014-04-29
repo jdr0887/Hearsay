6475afb4db3d6b370d276ca076ad3c585ec0a538
Jason Reilly <jdr0887@renci.org>
2014-04-29 10:37:18 -0400

    removing

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/opm/DocumentDAO.java
+++
@@ -1,8 +0,0 @@
-package org.renci.hearsay.dao.opm;
-
-import org.renci.hearsay.dao.BaseDAO;
-import org.renci.hearsay.dao.model.opm.Document;
-
-public interface DocumentDAO extends BaseDAO<Document, Long> {
-
-}
