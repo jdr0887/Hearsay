6f05376f2533d8f779ca990d8408219301291bb8
Jason Reilly <jdr0887@renci.org>
2014-04-29 10:37:18 -0400

    removing

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/opm/QualifiedNameDAO.java
+++
@@ -1,8 +0,0 @@
-package org.renci.hearsay.dao.opm;
-
-import org.renci.hearsay.dao.BaseDAO;
-import org.renci.hearsay.dao.model.opm.QualifiedName;
-
-public interface QualifiedNameDAO extends BaseDAO<QualifiedName, Long> {
-
-}
