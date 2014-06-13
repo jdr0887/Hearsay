d4899759ad35465ce8335d897ef9ac7ea2ba7d58
Jason Reilly <jdr0887@renci.org>
2014-06-13 14:18:56 -0400

    transcriptinterval rename

---
+++ hearsay-web-service-api/src/main/java/org/renci/hearsay/ws/MappedTranscriptService.java
@@ -0,0 +1,32 @@
+package org.renci.hearsay.ws;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.renci.hearsay.dao.model.MappedTranscript;
+
+@Path("/MappedTranscriptService/")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public interface MappedTranscriptService {
+
+    @GET
+    @Path("/findByTranscriptId/{id}")
+    public List<MappedTranscript> findByTranscriptId(@PathParam("id") Long id);
+
+    @GET
+    @Path("/findById/{id}")
+    public MappedTranscript findById(@PathParam("id") Long id);
+
+    @POST
+    @Path("/")
+    public Long save(MappedTranscript mappedTranscript);
+
+}
