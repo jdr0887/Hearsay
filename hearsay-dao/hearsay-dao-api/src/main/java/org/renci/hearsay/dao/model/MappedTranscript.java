6f1526179819109cff83d4b9c0e7797030011918
Jason Reilly <jdr0887@renci.org>
2014-06-23 09:38:47 -0400

    ignoring transcript

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/model/MappedTranscript.java
+++ hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/model/MappedTranscript.java
@@ -10,8 +10,10 @@ import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.XmlType;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
@@ -25,6 +27,7 @@ public class MappedTranscript extends BaseEntity {
 
     private static final long serialVersionUID = 1L;
 
+    @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "transcript_fid")
     private Transcript transcript;
