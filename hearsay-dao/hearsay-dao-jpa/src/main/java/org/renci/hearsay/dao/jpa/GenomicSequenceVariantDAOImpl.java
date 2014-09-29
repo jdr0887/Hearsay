e75453fa7d243bfe8c2b08c16edd1d3331f8274d
Jason Reilly <jdr0887@gmail.com>
2014-09-29 11:06:47 -0400

    changing name

--- hearsay-dao/hearsay-dao-jpa/src/main/java/org/renci/hearsay/dao/jpa/GenomicSequenceVariantDAOImpl.java
+++ hearsay-dao/hearsay-dao-jpa/src/main/java/org/renci/hearsay/dao/jpa/GenomicSequenceVariantDAOImpl.java
@@ -1,12 +1,12 @@
 package org.renci.hearsay.dao.jpa;
 
-import org.renci.hearsay.dao.GenomicSequenceVariantDAO;
+import org.renci.hearsay.dao.GenomicVariantDAO;
 import org.renci.hearsay.dao.model.GenomicSequenceVariant;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class GenomicSequenceVariantDAOImpl extends BaseEntityDAOImpl<GenomicSequenceVariant, Long> implements
-        GenomicSequenceVariantDAO {
+        GenomicVariantDAO {
 
     private final Logger logger = LoggerFactory.getLogger(GenomicSequenceVariantDAOImpl.class);
 
