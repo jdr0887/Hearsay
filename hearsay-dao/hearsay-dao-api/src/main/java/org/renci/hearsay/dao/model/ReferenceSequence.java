f7ca8eb707b329a2e5aca8f60eda4ba67ac97830
Jason Reilly <jdr0887@gmail.com>
2014-09-29 08:51:45 -0400

    cleaning

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/model/ReferenceSequence.java
+++ hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/model/ReferenceSequence.java
@@ -3,7 +3,6 @@ package org.renci.hearsay.dao.model;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
