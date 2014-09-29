acdabaf6ebc66f690884e6b90fee1994882f297a
Jason Reilly <jdr0887@gmail.com>
2014-09-29 10:34:53 -0400

    adding imple

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/model/ResolvedCondition.java
+++ hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/model/ResolvedCondition.java
@@ -37,4 +37,66 @@ public class ResolvedCondition extends BaseEntity {
         super();
     }
 
+    public Set<GeneCondition> getGeneConditions() {
+        return geneConditions;
+    }
+
+    public void setGeneConditions(Set<GeneCondition> geneConditions) {
+        this.geneConditions = geneConditions;
+    }
+
+    public Set<ConditionSubmission> getConditionSubmissions() {
+        return conditionSubmissions;
+    }
+
+    public void setConditionSubmissions(Set<ConditionSubmission> conditionSubmissions) {
+        this.conditionSubmissions = conditionSubmissions;
+    }
+
+    public Set<ResolvedConditionIdentifier> getResolvedConditionIdentifiers() {
+        return resolvedConditionIdentifiers;
+    }
+
+    public void setResolvedConditionIdentifiers(Set<ResolvedConditionIdentifier> resolvedConditionIdentifiers) {
+        this.resolvedConditionIdentifiers = resolvedConditionIdentifiers;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + ((conditionSubmissions == null) ? 0 : conditionSubmissions.hashCode());
+        result = prime * result + ((geneConditions == null) ? 0 : geneConditions.hashCode());
+        result = prime * result
+                + ((resolvedConditionIdentifiers == null) ? 0 : resolvedConditionIdentifiers.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!super.equals(obj))
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ResolvedCondition other = (ResolvedCondition) obj;
+        if (conditionSubmissions == null) {
+            if (other.conditionSubmissions != null)
+                return false;
+        } else if (!conditionSubmissions.equals(other.conditionSubmissions))
+            return false;
+        if (geneConditions == null) {
+            if (other.geneConditions != null)
+                return false;
+        } else if (!geneConditions.equals(other.geneConditions))
+            return false;
+        if (resolvedConditionIdentifiers == null) {
+            if (other.resolvedConditionIdentifiers != null)
+                return false;
+        } else if (!resolvedConditionIdentifiers.equals(other.resolvedConditionIdentifiers))
+            return false;
+        return true;
+    }
+
 }
