8ac79c10c90cb6c98ee46d213381874de18d2728
Jason Reilly <jdr0887@gmail.com>
2014-09-29 10:34:21 -0400

    adding getters/setters

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/model/GeneCondition.java
+++ hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/model/GeneCondition.java
@@ -1,9 +1,12 @@
 package org.renci.hearsay.dao.model;
 
-import javax.persistence.Column;
+import java.util.Set;
+
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
@@ -31,8 +34,35 @@ public class GeneCondition extends BaseEntity {
     @JoinColumn(name = "resolved_condition_fid")
     private ResolvedCondition resolvedCondition;
 
+    @OneToMany(mappedBy = "condition", fetch = FetchType.EAGER)
+    private Set<GeneConditionAssertion> assertions;
+
     public GeneCondition() {
         super();
+    }
+
+    public Gene getGene() {
+        return gene;
+    }
+
+    public void setGene(Gene gene) {
+        this.gene = gene;
+    }
+
+    public ResolvedCondition getResolvedCondition() {
+        return resolvedCondition;
+    }
+
+    public void setResolvedCondition(ResolvedCondition resolvedCondition) {
+        this.resolvedCondition = resolvedCondition;
+    }
+
+    public Set<GeneConditionAssertion> getAssertions() {
+        return assertions;
+    }
+
+    public void setAssertions(Set<GeneConditionAssertion> assertions) {
+        this.assertions = assertions;
     }
 
 }
