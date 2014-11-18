a71c1ab7dae64f2cd93ecb55125c268ffde72f0f
Jason Reilly <jdr0887@gmail.com>
2014-11-18 11:23:26 -0500

    synchronzing with online ref

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/model/TranscriptVariant.java
+++ hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/model/TranscriptVariant.java
@@ -31,4 +31,57 @@ public class TranscriptVariant extends VariantRepresentation {
         super();
     }
 
+    public String getReferenceAllele() {
+        return referenceAllele;
+    }
+
+    public void setReferenceAllele(String referenceAllele) {
+        this.referenceAllele = referenceAllele;
+    }
+
+    public String getVariantAllele() {
+        return variantAllele;
+    }
+
+    public void setVariantAllele(String variantAllele) {
+        this.variantAllele = variantAllele;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("TranscriptVariant [id=%s, referenceAllele=%s, variantAllele=%s]", id, referenceAllele,
+                variantAllele);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = super.hashCode();
+        result = prime * result + ((referenceAllele == null) ? 0 : referenceAllele.hashCode());
+        result = prime * result + ((variantAllele == null) ? 0 : variantAllele.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!super.equals(obj))
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        TranscriptVariant other = (TranscriptVariant) obj;
+        if (referenceAllele == null) {
+            if (other.referenceAllele != null)
+                return false;
+        } else if (!referenceAllele.equals(other.referenceAllele))
+            return false;
+        if (variantAllele == null) {
+            if (other.variantAllele != null)
+                return false;
+        } else if (!variantAllele.equals(other.variantAllele))
+            return false;
+        return true;
+    }
+
 }
