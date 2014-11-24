3d15278bc6f6d5b5892b90d444411691713842a7
Jason Reilly <jdr0887@gmail.com>
2014-11-24 12:33:51 -0500

    adding findByGeneName

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/CanonicalVariantDAO.java
+++ hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/CanonicalVariantDAO.java
@@ -1,7 +1,11 @@
 package org.renci.hearsay.dao;
 
+import java.util.List;
+
 import org.renci.hearsay.dao.model.CanonicalVariant;
 
 public interface CanonicalVariantDAO extends BaseEntityDAO<CanonicalVariant, Long> {
+
+    public abstract List<CanonicalVariant> findByGeneName(String name) throws HearsayDAOException;
 
 }
