5f7f51cb1e1cc46b70f9b846e1fc5c28635c8daf
Jason Reilly <jdr0887@gmail.com>
2014-12-10 10:15:38 -0500

    removing mapseq references

--- hearsay-dao/hearsay-dao-rs/src/main/java/org/renci/hearsay/dao/rs/TranscriptRefSeqDAOImpl.java
+++ hearsay-dao/hearsay-dao-rs/src/main/java/org/renci/hearsay/dao/rs/TranscriptRefSeqDAOImpl.java
@@ -1,4 +1,4 @@
-package edu.unc.mapseq.dao.rs;
+package org.renci.hearsay.dao.rs;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -8,36 +8,32 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.apache.cxf.jaxrs.client.WebClient;
+import org.renci.hearsay.dao.HearsayDAOException;
+import org.renci.hearsay.dao.TranscriptRefSeqDAO;
+import org.renci.hearsay.dao.model.TranscriptRefSeq;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import edu.unc.mapseq.config.MaPSeqConfigurationService;
-import edu.unc.mapseq.dao.FileDataDAO;
-import edu.unc.mapseq.dao.MaPSeqDAOException;
-import edu.unc.mapseq.dao.model.FileData;
 
 /**
  * 
  * @author jdr0887
  */
-public class FileDataDAOImpl extends BaseDAOImpl<FileData, Long> implements FileDataDAO {
+public class TranscriptRefSeqDAOImpl extends BaseEntityDAOImpl<TranscriptRefSeq, Long> implements TranscriptRefSeqDAO {
 
-    private final Logger logger = LoggerFactory.getLogger(FileDataDAOImpl.class);
+    private final Logger logger = LoggerFactory.getLogger(TranscriptRefSeqDAOImpl.class);
 
-    private MaPSeqConfigurationService configurationService;
-
-    public FileDataDAOImpl() {
-        super(FileData.class);
+    public TranscriptRefSeqDAOImpl() {
+        super(TranscriptRefSeq.class);
     }
 
     public void init() {
-        setRestServiceURL(String.format("http://%1$s:%2$d/cxf/%3$s/%3$sService", getConfigurationService()
-                .getWebServiceHost("localhost"), 8181, FileData.class.getSimpleName()));
+        setRestServiceURL(String.format("http://%1$s:%2$d/cxf/%3$s/%3$sService", getHost(), 8181,
+                TranscriptRefSeq.class.getSimpleName()));
     }
 
     @Override
-    public Long save(FileData entity) throws MaPSeqDAOException {
-        logger.debug("ENTERING save(FileData)");
+    public Long save(TranscriptRefSeq entity) throws HearsayDAOException {
+        logger.debug("ENTERING save(TranscriptRefSeq)");
         WebClient client = WebClient.create(getRestServiceURL()).type(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON);
         Response response = client.path("/").post(entity);
@@ -46,26 +42,45 @@ public class FileDataDAOImpl extends BaseDAOImpl<FileData, Long> implements File
     }
 
     @Override
-    public FileData findById(Long id) throws MaPSeqDAOException {
+    public TranscriptRefSeq findById(Long id) throws HearsayDAOException {
         WebClient client = WebClient.create(getRestServiceURL(), getProviders());
-        FileData fileData = client.path("findById/{id}", id).accept(MediaType.APPLICATION_JSON).get(FileData.class);
-        return fileData;
+        TranscriptRefSeq transcriptRefSeq = client.path("findById/{id}", id).accept(MediaType.APPLICATION_JSON)
+                .get(TranscriptRefSeq.class);
+        return transcriptRefSeq;
     }
 
     @Override
-    public List<FileData> findByExample(FileData fileData) {
-        WebClient client = WebClient.create(getRestServiceURL(), getProviders());
-        Collection<? extends FileData> ret = client.path("findByExample/{fileData}", fileData)
-                .accept(MediaType.APPLICATION_JSON).getCollection(FileData.class);
-        return new ArrayList<FileData>(ret);
+    public List<TranscriptRefSeq> findAll() throws HearsayDAOException {
+        return null;
     }
 
-    public MaPSeqConfigurationService getConfigurationService() {
-        return configurationService;
+    @Override
+    public List<TranscriptRefSeq> findByExample(TranscriptRefSeq transcriptRefSeq) throws HearsayDAOException {
+        return null;
     }
 
-    public void setConfigurationService(MaPSeqConfigurationService configurationService) {
-        this.configurationService = configurationService;
+    @Override
+    public List<TranscriptRefSeq> findByGeneId(Long geneId) throws HearsayDAOException {
+        WebClient client = WebClient.create(getRestServiceURL(), getProviders(), true);
+        Collection<? extends TranscriptRefSeq> results = client.path("findByGeneId/{geneId}", geneId)
+                .accept(MediaType.APPLICATION_JSON).getCollection(TranscriptRefSeq.class);
+        return new ArrayList<TranscriptRefSeq>(results);
+    }
+
+    @Override
+    public List<TranscriptRefSeq> findByGeneName(String name) throws HearsayDAOException {
+        WebClient client = WebClient.create(getRestServiceURL(), getProviders(), true);
+        Collection<? extends TranscriptRefSeq> results = client.path("findByGeneName/{geneName}", name)
+                .accept(MediaType.APPLICATION_JSON).getCollection(TranscriptRefSeq.class);
+        return new ArrayList<TranscriptRefSeq>(results);
+    }
+
+    @Override
+    public List<TranscriptRefSeq> findByAccession(String accession) throws HearsayDAOException {
+        WebClient client = WebClient.create(getRestServiceURL(), getProviders(), true);
+        Collection<? extends TranscriptRefSeq> results = client.path("findByAccession/{accession}", accession)
+                .accept(MediaType.APPLICATION_JSON).getCollection(TranscriptRefSeq.class);
+        return new ArrayList<TranscriptRefSeq>(results);
     }
 
 }
