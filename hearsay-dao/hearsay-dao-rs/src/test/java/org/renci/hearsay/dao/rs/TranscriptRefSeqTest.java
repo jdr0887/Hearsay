46c6e3900e925f903ec5c2e494574b4243fe9348
Jason Reilly <jdr0887@gmail.com>
2014-12-10 10:15:53 -0500

    removing mapseq references

--- hearsay-dao/hearsay-dao-rs/src/test/java/org/renci/hearsay/dao/rs/TranscriptRefSeqTest.java
+++ hearsay-dao/hearsay-dao-rs/src/test/java/org/renci/hearsay/dao/rs/TranscriptRefSeqTest.java
@@ -1,29 +1,69 @@
-package edu.unc.mapseq.dao.rs;
+package org.renci.hearsay.dao.rs;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
 
 import org.junit.Test;
+import org.renci.hearsay.dao.HearsayDAOException;
+import org.renci.hearsay.dao.model.Region;
+import org.renci.hearsay.dao.model.StrandType;
+import org.renci.hearsay.dao.model.TranscriptAlignment;
+import org.renci.hearsay.dao.model.TranscriptRefSeq;
 
-import edu.unc.mapseq.dao.MaPSeqDAOException;
-import edu.unc.mapseq.dao.model.HTSFSample;
-
-public class HTSFSampleTest {
+public class TranscriptRefSeqTest {
 
     @Test
-    public void testFindBySequencerRunId() {
-        RSDAOManager daoMgr = RSDAOManager.getInstance("edu/unc/mapseq/dao/rs/mapseq-dao-beans-test.xml");
+    public void testForContiguousRegions() {
+        RESTDAOManager daoMgr = RESTDAOManager.getInstance("org/renci/hearsay/dao/rs/hearsay-dao-beans-test.xml");
         try {
-            List<HTSFSample> entityList = daoMgr.getMaPSeqDAOBean().getHTSFSampleDAO().findBySequencerRunId(108078L);
-            if (entityList != null && entityList.size() > 0) {
-                for (HTSFSample entity : entityList) {
+            // List<TranscriptRefSeq> transcripts = daoMgr.getHearsayDAOBean().getTranscriptRefSeqDAO()
+            // .findByGeneName("DRD3");
+            // List<TranscriptRefSeq> transcripts = daoMgr.getHearsayDAOBean().getTranscriptRefSeqDAO()
+            // .findByGeneName("BRCA1");
+            // List<TranscriptRefSeq> transcripts = daoMgr.getHearsayDAOBean().getTranscriptRefSeqDAO()
+            // .findByGeneName("PRKAG2");
+            // List<TranscriptRefSeq> transcripts = daoMgr.getHearsayDAOBean().getTranscriptRefSeqDAO()
+            // .findByGeneName("PKP2");
+            List<TranscriptRefSeq> transcripts = daoMgr.getHearsayDAOBean().getTranscriptRefSeqDAO()
+                    .findByGeneName("PRSS1");
+
+            if (transcripts != null && !transcripts.isEmpty()) {
+                for (TranscriptRefSeq entity : transcripts) {
                     System.out.println(entity.toString());
+                    Set<TranscriptAlignment> alignments = entity.getAlignments();
+                    if (alignments != null && !alignments.isEmpty()) {
+                        for (TranscriptAlignment alignment : alignments) {
+                            System.out.println(alignment.toString());
+                            Set<Region> regions = alignment.getRegions();
+                            List<Region> sortedRegions = new ArrayList<Region>(regions);
+                            Collections.sort(sortedRegions, new Comparator<Region>() {
+                                @Override
+                                public int compare(Region o1, Region o2) {
+                                    return o1.getRegionStart().compareTo(o2.getRegionStart());
+                                }
+                            });
+
+                            if (sortedRegions != null && !sortedRegions.isEmpty()) {
+                                int previousRegionStop = 0;
+                                for (Region region : sortedRegions) {
+                                    System.out.println(region.toString());
+                                    if (previousRegionStop != 0) {
+                                        if (previousRegionStop + 1 != region.getRegionStart()) {
+                                            System.out.println("PROBLEM");
+                                        }
+                                    }
+                                    previousRegionStop = region.getRegionStop();
+                                }
+                            }
+                        }
+                    }
                 }
             }
-        } catch (MaPSeqDAOException e) {
+        } catch (HearsayDAOException e) {
             e.printStackTrace();
         }
-
     }
-
-
 }
