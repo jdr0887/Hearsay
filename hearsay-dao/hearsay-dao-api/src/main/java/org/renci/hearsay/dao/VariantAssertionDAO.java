9dfa0498a793f6135a9dfd5df09966d140a80a53
Jason Reilly <jdr0887@gmail.com>
2015-03-04 09:14:50 -0500

    adding findByGenomicVariantId(Long)

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/VariantAssertionDAO.java
+++ hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/VariantAssertionDAO.java
@@ -1,7 +1,11 @@
 package org.renci.hearsay.dao;
 
+import java.util.List;
+
 import org.renci.hearsay.dao.model.VariantAssertion;
 
 public interface VariantAssertionDAO extends BaseEntityDAO<VariantAssertion, Long> {
+
+    public List<VariantAssertion> findByGenomicVariantId(Long genomicVariantId) throws HearsayDAOException;
 
 }
