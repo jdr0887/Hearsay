60eda5381d68563986fdf7b5d077c2c3b3f6db34
Jason Reilly <jdr0887@gmail.com>
2015-04-17 14:03:40 -0400

    removing

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/ResolvedConditionDAO.java
+++
@@ -1,7 +0,0 @@
-package org.renci.hearsay.dao;
-
-import org.renci.hearsay.dao.model.ResolvedCondition;
-
-public interface ResolvedConditionDAO extends BaseEntityDAO<ResolvedCondition, Long> {
-
-}
