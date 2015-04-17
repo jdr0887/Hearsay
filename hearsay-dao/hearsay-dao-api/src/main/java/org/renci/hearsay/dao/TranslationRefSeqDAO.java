0d559e944e23dec670b41d97b314916646346dcb
Jason Reilly <jdr0887@gmail.com>
2015-04-17 14:03:40 -0400

    removing

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/TranslationRefSeqDAO.java
+++
@@ -1,7 +0,0 @@
-package org.renci.hearsay.dao;
-
-import org.renci.hearsay.dao.model.TranslationRefSeq;
-
-public interface TranslationRefSeqDAO extends BaseEntityDAO<TranslationRefSeq, Long> {
-
-}
