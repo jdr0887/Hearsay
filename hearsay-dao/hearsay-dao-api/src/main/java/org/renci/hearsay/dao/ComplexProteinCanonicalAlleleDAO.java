e4da6a7814edbb1dedb06f3ead5e16b7b5bb3283
Jason Reilly <jdr0887@gmail.com>
2015-04-20 16:13:34 -0400

    removing

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/ComplexProteinCanonicalAlleleDAO.java
+++
@@ -1,7 +0,0 @@
-package org.renci.hearsay.dao;
-
-import org.renci.hearsay.dao.model.ComplexProteinCanonicalAllele;
-
-public interface ComplexProteinCanonicalAlleleDAO extends BaseEntityDAO<ComplexProteinCanonicalAllele, Long> {
-
-}
