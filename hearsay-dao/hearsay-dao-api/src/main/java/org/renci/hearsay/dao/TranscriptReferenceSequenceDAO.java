90e318416f519459fcd153693f035f7c9a443d35
Jason Reilly <jdr0887@gmail.com>
2015-05-18 09:20:35 -0400

    posterity commit

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/TranscriptReferenceSequenceDAO.java
+++ hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/TranscriptReferenceSequenceDAO.java
@@ -1,7 +1,14 @@
 package org.renci.hearsay.dao;
 
+import java.util.List;
+
 import org.renci.hearsay.dao.model.TranscriptReferenceSequence;
 
 public interface TranscriptReferenceSequenceDAO extends BaseEntityDAO<TranscriptReferenceSequence, Long> {
+
+    public List<TranscriptReferenceSequence> findByExample(TranscriptReferenceSequence transcriptReferenceSequence)
+            throws HearsayDAOException;
+
+    public List<TranscriptReferenceSequence> findByGeneId(Long geneId) throws HearsayDAOException;
 
 }
