b8c7fa33f46734e3118a70e92c17e59555f2edf5
Jason Reilly <jdr0887@renci.org>
2015-05-21 08:29:43 -0400

    removing

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/SimpleTranscriptAlleleDAO.java
+++
@@ -1,7 +0,0 @@
-package org.renci.hearsay.dao;
-
-import org.renci.hearsay.dao.model.SimpleTranscriptAllele;
-
-public interface SimpleTranscriptAlleleDAO extends BaseEntityDAO<SimpleTranscriptAllele, Long> {
-
-}
