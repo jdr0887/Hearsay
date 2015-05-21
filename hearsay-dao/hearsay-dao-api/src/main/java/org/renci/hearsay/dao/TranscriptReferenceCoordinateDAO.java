033b095cfaade32ee4200aa0911cc262be4299ac
Jason Reilly <jdr0887@renci.org>
2015-05-21 08:29:43 -0400

    removing

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/TranscriptReferenceCoordinateDAO.java
+++
@@ -1,7 +0,0 @@
-package org.renci.hearsay.dao;
-
-import org.renci.hearsay.dao.model.TranscriptReferenceCoordinate;
-
-public interface TranscriptReferenceCoordinateDAO extends BaseEntityDAO<TranscriptReferenceCoordinate, Long> {
-
-}
