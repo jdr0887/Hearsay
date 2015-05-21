387f24f8e882ed628795f117b02127bb739e8725
Jason Reilly <jdr0887@renci.org>
2015-05-21 08:29:42 -0400

    removing

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/SimpleAminoAcidCanonicalAlleleDAO.java
+++
@@ -1,7 +0,0 @@
-package org.renci.hearsay.dao;
-
-import org.renci.hearsay.dao.model.SimpleAminoAcidCanonicalAllele;
-
-public interface SimpleAminoAcidCanonicalAlleleDAO extends BaseEntityDAO<SimpleAminoAcidCanonicalAllele, Long> {
-
-}
