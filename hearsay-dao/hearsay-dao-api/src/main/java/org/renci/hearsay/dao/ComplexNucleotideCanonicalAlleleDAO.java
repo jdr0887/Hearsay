5c8802f8df955f583f22220948b3c9dbddfbafca
Jason Reilly <jdr0887@renci.org>
2015-05-21 08:29:42 -0400

    removing

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/ComplexNucleotideCanonicalAlleleDAO.java
+++
@@ -1,7 +0,0 @@
-package org.renci.hearsay.dao;
-
-import org.renci.hearsay.dao.model.ComplexNucleotideCanonicalAllele;
-
-public interface ComplexNucleotideCanonicalAlleleDAO extends BaseEntityDAO<ComplexNucleotideCanonicalAllele, Long> {
-
-}
