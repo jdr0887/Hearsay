20017ed1df9ba8b8017d6286ac097b0655183940
Jason Reilly <jdr0887@renci.org>
2015-05-21 08:29:42 -0400

    removing

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/ChromosomeReferenceSequenceDAO.java
+++
@@ -1,7 +0,0 @@
-package org.renci.hearsay.dao;
-
-import org.renci.hearsay.dao.model.ChromosomeReferenceSequence;
-
-public interface ChromosomeReferenceSequenceDAO extends BaseEntityDAO<ChromosomeReferenceSequence, Long> {
-
-}
