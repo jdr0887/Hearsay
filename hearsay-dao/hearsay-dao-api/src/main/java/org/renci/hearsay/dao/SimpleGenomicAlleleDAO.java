10ac35d69161d6f34cd35bc93b6b3838e3c02d03
Jason Reilly <jdr0887@renci.org>
2015-05-21 08:29:42 -0400

    removing

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/SimpleGenomicAlleleDAO.java
+++
@@ -1,7 +0,0 @@
-package org.renci.hearsay.dao;
-
-import org.renci.hearsay.dao.model.SimpleGenomicAllele;
-
-public interface SimpleGenomicAlleleDAO extends BaseEntityDAO<SimpleGenomicAllele, Long> {
-
-}
