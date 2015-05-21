02696afb5f9289bfb63fdc8f86b338e51f851704
Jason Reilly <jdr0887@renci.org>
2015-05-21 08:29:42 -0400

    removing

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/GenomicReferenceSequenceDAO.java
+++
@@ -1,7 +0,0 @@
-package org.renci.hearsay.dao;
-
-import org.renci.hearsay.dao.model.GenomicReferenceSequence;
-
-public interface GenomicReferenceSequenceDAO extends BaseEntityDAO<GenomicReferenceSequence, Long> {
-
-}
