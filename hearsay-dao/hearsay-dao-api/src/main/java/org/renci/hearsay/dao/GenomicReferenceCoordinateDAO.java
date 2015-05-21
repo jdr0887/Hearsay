ce8c5902a7747814f07a2428f563dbfc139b59dd
Jason Reilly <jdr0887@renci.org>
2015-05-21 08:29:42 -0400

    removing

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/GenomicReferenceCoordinateDAO.java
+++
@@ -1,7 +0,0 @@
-package org.renci.hearsay.dao;
-
-import org.renci.hearsay.dao.model.GenomicReferenceCoordinate;
-
-public interface GenomicReferenceCoordinateDAO extends BaseEntityDAO<GenomicReferenceCoordinate, Long> {
-
-}
