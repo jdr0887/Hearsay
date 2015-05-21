1fb7102600f0dd3366822e18ade184693b4111b2
Jason Reilly <jdr0887@renci.org>
2015-05-21 08:29:43 -0400

    removing

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/SimpleNucleotideCanonicalAlleleDAO.java
+++
@@ -1,7 +0,0 @@
-package org.renci.hearsay.dao;
-
-import org.renci.hearsay.dao.model.SimpleNucleotideCanonicalAllele;
-
-public interface SimpleNucleotideCanonicalAlleleDAO extends BaseEntityDAO<SimpleNucleotideCanonicalAllele, Long> {
-
-}
