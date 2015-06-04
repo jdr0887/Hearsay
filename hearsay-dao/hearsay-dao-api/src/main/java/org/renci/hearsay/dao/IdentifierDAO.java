7e5bb697c9e402ee6a79730bb119500a85665603
Jason Reilly <jdr0887@gmail.com>
2015-06-04 15:57:14 -0400

    adding findByExample

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/IdentifierDAO.java
+++ hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/IdentifierDAO.java
@@ -1,7 +1,12 @@
 package org.renci.hearsay.dao;
 
+import java.util.List;
+
+import org.renci.hearsay.dao.model.Gene;
 import org.renci.hearsay.dao.model.Identifier;
 
 public interface IdentifierDAO extends BaseEntityDAO<Identifier, Long> {
+
+    public List<Identifier> findByExample(Identifier identifier) throws HearsayDAOException;
 
 }
