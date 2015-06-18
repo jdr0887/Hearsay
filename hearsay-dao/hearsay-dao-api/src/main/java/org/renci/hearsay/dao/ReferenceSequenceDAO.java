7708e1943d54f6be1b95ff009447cc53f596c51a
Jason Reilly <jdr0887@gmail.com>
2015-06-18 11:30:19 -0400

    adding accessors

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/ReferenceSequenceDAO.java
+++ hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/ReferenceSequenceDAO.java
@@ -1,7 +1,15 @@
 package org.renci.hearsay.dao;
 
+import java.util.List;
+
 import org.renci.hearsay.dao.model.ReferenceSequence;
 
 public interface ReferenceSequenceDAO extends BaseEntityDAO<ReferenceSequence, Long> {
+
+    public List<ReferenceSequence> findByGeneId(Long geneId) throws HearsayDAOException;
+
+    public List<ReferenceSequence> findAll() throws HearsayDAOException;
+
+    public List<ReferenceSequence> findByIdentifierValue(String value) throws HearsayDAOException;
 
 }
