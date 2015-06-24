002a52875199f54d4215108fa92844531e700bed
Jason Reilly <jdr0887@gmail.com>
2015-06-24 13:32:12 -0400

    cleaning imports

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/IdentifierDAO.java
+++ hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/IdentifierDAO.java
@@ -2,7 +2,6 @@ package org.renci.hearsay.dao;
 
 import java.util.List;
 
-import org.renci.hearsay.dao.model.Gene;
 import org.renci.hearsay.dao.model.Identifier;
 
 public interface IdentifierDAO extends BaseEntityDAO<Identifier, Long> {
