7cf70ef32a8f21bac6d8dbed87ba978272bf347e
Jason Reilly <jdr0887@gmail.com>
2015-06-24 13:31:59 -0400

    adding new entities

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/HearsayDAOBean.java
+++ hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/HearsayDAOBean.java
@@ -2,7 +2,11 @@ package org.renci.hearsay.dao;
 
 public class HearsayDAOBean {
 
+    private AlignmentDAO alignmentDAO;
+
     private CanonicalAlleleDAO canonicalAlleleDAO;
+
+    private FeatureDAO featureDAO;
 
     private GeneDAO geneDAO;
 
@@ -12,7 +16,15 @@ public class HearsayDAOBean {
 
     private IntronOffsetDAO intronOffsetDAO;
 
+    private LocationDAO locationDAO;
+
+    private MolecularConsequenceDAO molecularConsequenceDAO;
+
+    private PopulationFrequencyDAO populationFrequencyDAO;
+
     private ReferenceCoordinateDAO referenceCoordinateDAO;
+
+    private RegionDAO regionDAO;
 
     private GenomeReferenceDAO genomeReferenceDAO;
 
@@ -24,12 +36,28 @@ public class HearsayDAOBean {
         super();
     }
 
+    public AlignmentDAO getAlignmentDAO() {
+        return alignmentDAO;
+    }
+
+    public void setAlignmentDAO(AlignmentDAO alignmentDAO) {
+        this.alignmentDAO = alignmentDAO;
+    }
+
     public CanonicalAlleleDAO getCanonicalAlleleDAO() {
         return canonicalAlleleDAO;
     }
 
     public void setCanonicalAlleleDAO(CanonicalAlleleDAO canonicalAlleleDAO) {
         this.canonicalAlleleDAO = canonicalAlleleDAO;
+    }
+
+    public FeatureDAO getFeatureDAO() {
+        return featureDAO;
+    }
+
+    public void setFeatureDAO(FeatureDAO featureDAO) {
+        this.featureDAO = featureDAO;
     }
 
     public GeneDAO getGeneDAO() {
@@ -64,12 +92,44 @@ public class HearsayDAOBean {
         this.intronOffsetDAO = intronOffsetDAO;
     }
 
+    public LocationDAO getLocationDAO() {
+        return locationDAO;
+    }
+
+    public void setLocationDAO(LocationDAO locationDAO) {
+        this.locationDAO = locationDAO;
+    }
+
+    public MolecularConsequenceDAO getMolecularConsequenceDAO() {
+        return molecularConsequenceDAO;
+    }
+
+    public void setMolecularConsequenceDAO(MolecularConsequenceDAO molecularConsequenceDAO) {
+        this.molecularConsequenceDAO = molecularConsequenceDAO;
+    }
+
+    public PopulationFrequencyDAO getPopulationFrequencyDAO() {
+        return populationFrequencyDAO;
+    }
+
+    public void setPopulationFrequencyDAO(PopulationFrequencyDAO populationFrequencyDAO) {
+        this.populationFrequencyDAO = populationFrequencyDAO;
+    }
+
     public ReferenceCoordinateDAO getReferenceCoordinateDAO() {
         return referenceCoordinateDAO;
     }
 
     public void setReferenceCoordinateDAO(ReferenceCoordinateDAO referenceCoordinateDAO) {
         this.referenceCoordinateDAO = referenceCoordinateDAO;
+    }
+
+    public RegionDAO getRegionDAO() {
+        return regionDAO;
+    }
+
+    public void setRegionDAO(RegionDAO regionDAO) {
+        this.regionDAO = regionDAO;
     }
 
     public GenomeReferenceDAO getGenomeReferenceDAO() {
