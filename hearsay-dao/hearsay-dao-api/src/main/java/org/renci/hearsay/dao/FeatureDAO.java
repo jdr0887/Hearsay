466d50241d72c152d1a5109cb378e859afd11d4e
Jason Reilly <jdr0887@gmail.com>
2015-07-08 13:43:02 -0400

    removing findByReferenceSequenceId

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/FeatureDAO.java
+++ hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/FeatureDAO.java
@@ -1,11 +1,7 @@
 package org.renci.hearsay.dao;
 
-import java.util.List;
-
 import org.renci.hearsay.dao.model.Feature;
 
 public interface FeatureDAO extends BaseEntityDAO<Feature, Long> {
-
-    public List<Feature> findByReferenceSequenceId(Long id) throws HearsayDAOException;
 
 }
