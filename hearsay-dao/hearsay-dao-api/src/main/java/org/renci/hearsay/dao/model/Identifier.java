1f2c84fa17aac6b8be6d916ed86e70de035adbb4
Jason Reilly <jdr0887@gmail.com>
2015-07-08 13:42:17 -0400

    posterity commit

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/model/Identifier.java
+++ hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/model/Identifier.java
@@ -20,7 +20,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 @JsonInclude(Include.NON_EMPTY)
-@XmlType(name = "Identifier", propOrder = { "id", "system", "value" })
+@XmlType(propOrder = { "id", "system", "value" })
 @XmlRootElement(name = "identifier")
 @XmlAccessorType(XmlAccessType.FIELD)
 @Entity
