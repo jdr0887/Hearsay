3dad0faaa00ca7f7a03d4608649c8e8917c2ae67
Jason Reilly <jdr0887@gmail.com>
2015-07-27 14:27:36 -0400

    adding findByExample & findByIdentifiers

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/ReferenceSequenceDAO.java
+++ hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/ReferenceSequenceDAO.java
@@ -2,17 +2,18 @@ package org.renci.hearsay.dao;
 
 import java.util.List;
 
-import org.renci.hearsay.dao.model.Identifier;
 import org.renci.hearsay.dao.model.ReferenceSequence;
 
 public interface ReferenceSequenceDAO extends BaseEntityDAO<ReferenceSequence, Long> {
 
     public List<ReferenceSequence> findByGeneId(Long geneId) throws HearsayDAOException;
 
+    public List<ReferenceSequence> findByExample(ReferenceSequence referenceSequence) throws HearsayDAOException;
+
     public List<ReferenceSequence> findAll() throws HearsayDAOException;
 
     public List<ReferenceSequence> findByIdentifierValue(String value) throws HearsayDAOException;
 
-    public List<ReferenceSequence> findByIdentifiers(Identifier... identifiers) throws HearsayDAOException;
+    public List<ReferenceSequence> findByIdentifiers(List<Long> idList) throws HearsayDAOException;
 
 }
