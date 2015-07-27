ee2b35ef7e10caf9d0cf1ee570d03eb109e9183d
Jason Reilly <jdr0887@gmail.com>
2015-07-27 14:28:42 -0400

    adding id

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/model/Chromosome.java
+++ hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/model/Chromosome.java
@@ -2,8 +2,12 @@ package org.renci.hearsay.dao.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 import javax.xml.bind.annotation.XmlAccessType;
@@ -13,6 +17,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
 import org.apache.openjpa.persistence.jdbc.Index;
+import org.renci.hearsay.dao.Persistable;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
@@ -24,9 +29,16 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 @Entity
 @Table(name = "chromosome", uniqueConstraints = { @UniqueConstraint(columnNames = { "name" }) })
 @NamedQueries({ @NamedQuery(name = "Chromosome.findByName", query = "SELECT a FROM Chromosome a where a.name = :name") })
-public class Chromosome extends DictionaryEntity {
+public class Chromosome implements Persistable {
 
     private static final long serialVersionUID = 2931123376533316569L;
+
+    @XmlAttribute(name = "id")
+    @Id()
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "chromosome_id_seq")
+    @SequenceGenerator(name = "chromosome_id_seq", sequenceName = "chromosome_id_seq", allocationSize = 1, initialValue = 1)
+    @Column(name = "id")
+    private Long id;
 
     @XmlAttribute
     @Index
@@ -35,6 +47,14 @@ public class Chromosome extends DictionaryEntity {
 
     public Chromosome() {
         super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public Chromosome(String name) {
@@ -57,7 +77,8 @@ public class Chromosome extends DictionaryEntity {
     @Override
     public int hashCode() {
         final int prime = 31;
-        int result = super.hashCode();
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
         result = prime * result + ((name == null) ? 0 : name.hashCode());
         return result;
     }
@@ -66,11 +87,16 @@ public class Chromosome extends DictionaryEntity {
     public boolean equals(Object obj) {
         if (this == obj)
             return true;
-        if (!super.equals(obj))
+        if (obj == null)
             return false;
         if (getClass() != obj.getClass())
             return false;
         Chromosome other = (Chromosome) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
         if (name == null) {
             if (other.name != null)
                 return false;
