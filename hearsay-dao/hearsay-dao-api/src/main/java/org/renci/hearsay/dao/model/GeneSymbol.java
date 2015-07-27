63d899613e13add441495b4cb35b143f6bf979d7
Jason Reilly <jdr0887@gmail.com>
2015-07-27 14:28:55 -0400

    adding id

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/model/GeneSymbol.java
+++ hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/model/GeneSymbol.java
@@ -2,8 +2,12 @@ package org.renci.hearsay.dao.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
@@ -13,6 +17,7 @@ import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.XmlType;
 
 import org.apache.openjpa.persistence.jdbc.Index;
+import org.renci.hearsay.dao.Persistable;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
@@ -23,9 +28,16 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 @XmlAccessorType(XmlAccessType.FIELD)
 @Entity
 @Table(name = "gene_symbol")
-public class GeneSymbol extends DictionaryEntity {
+public class GeneSymbol implements Persistable {
 
     private static final long serialVersionUID = -5997799315221166517L;
+
+    @XmlAttribute(name = "id")
+    @Id()
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "gene_symbol_id_seq")
+    @SequenceGenerator(name = "gene_symbol_id_seq", sequenceName = "gene_symbol_id_seq", allocationSize = 1, initialValue = 1)
+    @Column(name = "id")
+    private Long id;
 
     @XmlAttribute
     @Index
@@ -39,6 +51,14 @@ public class GeneSymbol extends DictionaryEntity {
 
     public GeneSymbol() {
         super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public Gene getGene() {
@@ -65,7 +85,8 @@ public class GeneSymbol extends DictionaryEntity {
     @Override
     public int hashCode() {
         final int prime = 31;
-        int result = super.hashCode();
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
         result = prime * result + ((symbol == null) ? 0 : symbol.hashCode());
         return result;
     }
@@ -74,11 +95,16 @@ public class GeneSymbol extends DictionaryEntity {
     public boolean equals(Object obj) {
         if (this == obj)
             return true;
-        if (!super.equals(obj))
+        if (obj == null)
             return false;
         if (getClass() != obj.getClass())
             return false;
         GeneSymbol other = (GeneSymbol) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
         if (symbol == null) {
             if (other.symbol != null)
                 return false;
