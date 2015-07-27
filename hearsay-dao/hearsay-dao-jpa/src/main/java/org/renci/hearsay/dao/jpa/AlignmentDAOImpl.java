6bd3c65a71256ea5bb4e1493bab30c37032a8654
Jason Reilly <jdr0887@gmail.com>
2015-07-27 14:26:16 -0400

    findByReferenceSequenceId(Long)

--- hearsay-dao/hearsay-dao-jpa/src/main/java/org/renci/hearsay/dao/jpa/AlignmentDAOImpl.java
+++ hearsay-dao/hearsay-dao-jpa/src/main/java/org/renci/hearsay/dao/jpa/AlignmentDAOImpl.java
@@ -1,7 +1,21 @@
 package org.renci.hearsay.dao.jpa;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
 import org.renci.hearsay.dao.AlignmentDAO;
+import org.renci.hearsay.dao.HearsayDAOException;
 import org.renci.hearsay.dao.model.Alignment;
+import org.renci.hearsay.dao.model.Alignment_;
+import org.renci.hearsay.dao.model.ReferenceSequence;
+import org.renci.hearsay.dao.model.ReferenceSequence_;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,6 +30,23 @@ public class AlignmentDAOImpl extends BaseEntityDAOImpl<Alignment, Long> impleme
     @Override
     public Class<Alignment> getPersistentClass() {
         return Alignment.class;
+    }
+
+    @Override
+    public List<Alignment> findByReferenceSequenceId(Long referenceSequenceId) throws HearsayDAOException {
+        logger.debug("ENTERING findByReferenceSequenceId(Long)");
+        CriteriaBuilder critBuilder = getEntityManager().getCriteriaBuilder();
+        CriteriaQuery<Alignment> crit = critBuilder.createQuery(getPersistentClass());
+        Root<Alignment> fromAlignment = crit.from(Alignment.class);
+        List<Predicate> predicates = new ArrayList<Predicate>();
+        Join<Alignment, ReferenceSequence> alignmentReferenceSequenceJoin = fromAlignment
+                .join(Alignment_.referenceSequences);
+        predicates
+                .add(critBuilder.equal(alignmentReferenceSequenceJoin.get(ReferenceSequence_.id), referenceSequenceId));
+        crit.where(predicates.toArray(new Predicate[predicates.size()]));
+        TypedQuery<Alignment> query = getEntityManager().createQuery(crit);
+        List<Alignment> ret = query.getResultList();
+        return ret;
     }
 
 }
--- hearsay-dao/hearsay-dao-jpa/src/main/java/org/renci/hearsay/dao/jpa/FeatureDAOImpl.java
+++ hearsay-dao/hearsay-dao-jpa/src/main/java/org/renci/hearsay/dao/jpa/FeatureDAOImpl.java
@@ -1,7 +1,21 @@
 package org.renci.hearsay.dao.jpa;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
 import org.renci.hearsay.dao.FeatureDAO;
+import org.renci.hearsay.dao.HearsayDAOException;
 import org.renci.hearsay.dao.model.Feature;
+import org.renci.hearsay.dao.model.Feature_;
+import org.renci.hearsay.dao.model.ReferenceSequence;
+import org.renci.hearsay.dao.model.ReferenceSequence_;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,6 +30,21 @@ public class FeatureDAOImpl extends BaseEntityDAOImpl<Feature, Long> implements 
     @Override
     public Class<Feature> getPersistentClass() {
         return Feature.class;
+    }
+
+    @Override
+    public List<Feature> findByReferenceSequenceId(Long referenceSequenceId) throws HearsayDAOException {
+        logger.debug("ENTERING findByReferenceSequenceId(Long)");
+        CriteriaBuilder critBuilder = getEntityManager().getCriteriaBuilder();
+        CriteriaQuery<Feature> crit = critBuilder.createQuery(getPersistentClass());
+        Root<Feature> fromFeature = crit.from(Feature.class);
+        List<Predicate> predicates = new ArrayList<Predicate>();
+        Join<Feature, ReferenceSequence> featureReferenceSequenceJoin = fromFeature.join(Feature_.referenceSequences);
+        predicates.add(critBuilder.equal(featureReferenceSequenceJoin.get(ReferenceSequence_.id), referenceSequenceId));
+        crit.where(predicates.toArray(new Predicate[predicates.size()]));
+        TypedQuery<Feature> query = getEntityManager().createQuery(crit);
+        List<Feature> ret = query.getResultList();
+        return ret;
     }
 
 }
