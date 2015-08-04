327e46e59568a0a22506baa4d28059483afb9b27
Jason Reilly <jdr0887@gmail.com>
2015-08-04 10:24:48 -0400

    adding pathparam

--- hearsay-web-service-api/src/main/java/org/renci/hearsay/ws/FeatureService.java
+++ hearsay-web-service-api/src/main/java/org/renci/hearsay/ws/FeatureService.java
@@ -23,6 +23,7 @@ public interface FeatureService {
 
     @GET
     @Path("/findByReferenceSequenceId/{referenceSequenceId}")
-    public List<Feature> findByReferenceSequenceId(Long referenceSequenceId) throws HearsayDAOException;
+    public List<Feature> findByReferenceSequenceId(@PathParam("referenceSequenceId") Long referenceSequenceId)
+            throws HearsayDAOException;
 
 }
