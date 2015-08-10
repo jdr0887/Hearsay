88d20190322bf8dc6ab5dd6b47934b97f1301312
Jason Reilly <jdr0887@gmail.com>
2015-08-10 16:06:05 -0400

    adding testCanonicalAlleleSerialization

--- hearsay-dao/hearsay-dao-jpa/src/test/java/org/renci/hearsay/dao/jpa/CanonicalVariantTest.java
+++ hearsay-dao/hearsay-dao-jpa/src/test/java/org/renci/hearsay/dao/jpa/CanonicalVariantTest.java
@@ -1,16 +1,24 @@
 package org.renci.hearsay.dao.jpa;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.PropertyException;
 
 import org.apache.commons.collections.CollectionUtils;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.renci.hearsay.dao.HearsayDAOException;
+import org.renci.hearsay.dao.model.CanonicalAllele;
 import org.renci.hearsay.dao.model.Gene;
 
 public class CanonicalVariantTest {
@@ -66,6 +74,24 @@ public class CanonicalVariantTest {
         // }
         // }
         // }
+
+    }
+
+    @Test
+    public void testCanonicalAlleleSerialization() {
+
+        try {
+            CanonicalAlleleDAOImpl canonicalVariantDAO = new CanonicalAlleleDAOImpl();
+            canonicalVariantDAO.setEntityManager(em);
+
+            JAXBContext context = JAXBContext.newInstance(CanonicalAllele.class);
+            Marshaller m = context.createMarshaller();
+            m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            FileWriter fw = new FileWriter(new File("/tmp/canonicalAllele.xml"));
+            m.marshal(canonicalVariantDAO.findById(94483L), fw);
+        } catch (JAXBException | IOException | HearsayDAOException e) {
+            e.printStackTrace();
+        }
 
     }
 
