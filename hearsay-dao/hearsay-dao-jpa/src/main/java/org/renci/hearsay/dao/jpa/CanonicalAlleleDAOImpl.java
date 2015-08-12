ffaacb5431bbe8570fd1b01cc8470d3f7091f83e
Jason Reilly <jdr0887@gmail.com>
2015-08-12 13:20:33 -0400

    removing logger

--- hearsay-dao/hearsay-dao-jpa/src/main/java/org/renci/hearsay/dao/jpa/CanonicalAlleleDAOImpl.java
+++ hearsay-dao/hearsay-dao-jpa/src/main/java/org/renci/hearsay/dao/jpa/CanonicalAlleleDAOImpl.java
@@ -2,12 +2,8 @@ package org.renci.hearsay.dao.jpa;
 
 import org.renci.hearsay.dao.CanonicalAlleleDAO;
 import org.renci.hearsay.dao.model.CanonicalAllele;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class CanonicalAlleleDAOImpl extends BaseEntityDAOImpl<CanonicalAllele, Long> implements CanonicalAlleleDAO {
-
-    private final Logger logger = LoggerFactory.getLogger(CanonicalAlleleDAOImpl.class);
 
     public CanonicalAlleleDAOImpl() {
         super();
