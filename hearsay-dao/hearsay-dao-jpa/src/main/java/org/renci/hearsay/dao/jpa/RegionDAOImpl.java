5164dede84eb8de21f13b8c91cd84aa5ebfd630b
Jason Reilly <jdr0887@gmail.com>
2015-08-12 13:20:56 -0400

    removing logger

--- hearsay-dao/hearsay-dao-jpa/src/main/java/org/renci/hearsay/dao/jpa/RegionDAOImpl.java
+++ hearsay-dao/hearsay-dao-jpa/src/main/java/org/renci/hearsay/dao/jpa/RegionDAOImpl.java
@@ -2,12 +2,8 @@ package org.renci.hearsay.dao.jpa;
 
 import org.renci.hearsay.dao.RegionDAO;
 import org.renci.hearsay.dao.model.Region;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class RegionDAOImpl extends BaseEntityDAOImpl<Region, Long> implements RegionDAO {
-
-    private final Logger logger = LoggerFactory.getLogger(RegionDAOImpl.class);
 
     public RegionDAOImpl() {
         super();
