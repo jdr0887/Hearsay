d72680a52f1b731f58591da51ac924119ba73d1a
Jason Reilly <jdr0887@gmail.com>
2015-10-15 13:35:14 -0400

    adding transactional

--- hearsay-dao/hearsay-dao-jpa/src/main/java/org/renci/hearsay/dao/jpa/BaseEntityDAOImpl.java
+++ hearsay-dao/hearsay-dao-jpa/src/main/java/org/renci/hearsay/dao/jpa/BaseEntityDAOImpl.java
@@ -6,7 +6,10 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceUnit;
-import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaDelete;
+import javax.persistence.criteria.Root;
+import javax.transaction.Transactional;
 
 import org.renci.hearsay.dao.BaseEntityDAO;
 import org.renci.hearsay.dao.HearsayDAOException;
@@ -14,9 +17,10 @@ import org.renci.hearsay.dao.Persistable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public abstract class BaseEntityDAOImpl<T extends Persistable, ID extends Serializable> implements BaseEntityDAO<T, ID> {
+public abstract class BaseEntityDAOImpl<T extends Persistable, ID extends Serializable>
+        implements BaseEntityDAO<T, ID> {
 
-    private final Logger logger = LoggerFactory.getLogger(BaseEntityDAOImpl.class);
+    private static final Logger logger = LoggerFactory.getLogger(BaseEntityDAOImpl.class);
 
     @PersistenceUnit(name = "hearsay", unitName = "hearsay")
     private EntityManager entityManager;
@@ -28,6 +32,7 @@ public abstract class BaseEntityDAOImpl<T extends Persistable, ID extends Serial
     public abstract Class<T> getPersistentClass();
 
     @Override
+    @Transactional
     public Long save(T entity) throws HearsayDAOException {
         logger.debug("ENTERING save(T)");
         if (!entityManager.contains(entity) && entity.getId() != null) {
@@ -39,6 +44,7 @@ public abstract class BaseEntityDAOImpl<T extends Persistable, ID extends Serial
     }
 
     @Override
+    @Transactional
     public void delete(T entity) throws HearsayDAOException {
         logger.debug("ENTERING delete(T)");
         T foundEntity = entityManager.find(getPersistentClass(), entity.getId());
@@ -46,19 +52,22 @@ public abstract class BaseEntityDAOImpl<T extends Persistable, ID extends Serial
     }
 
     @Override
+    @Transactional
     public void delete(List<T> entityList) throws HearsayDAOException {
         logger.debug("ENTERING delete(List<T>)");
         List<Long> idList = new ArrayList<Long>();
         for (T t : entityList) {
             idList.add(t.getId());
         }
-        Query qDelete = entityManager.createQuery("delete from " + getPersistentClass().getSimpleName()
-                + " a where a.id in (?1)");
-        qDelete.setParameter(1, idList);
-        qDelete.executeUpdate();
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaDelete<T> criteriaDelete = criteriaBuilder.createCriteriaDelete(getPersistentClass());
+        Root<T> root = criteriaDelete.from(getPersistentClass());
+        criteriaDelete.where(root.<Long> get("id").in(idList));
+        entityManager.createQuery(criteriaDelete).executeUpdate();
     }
 
     @Override
+    @Transactional
     public T findById(ID id) throws HearsayDAOException {
         logger.debug("ENTERING findById(T)");
         T ret = entityManager.find(getPersistentClass(), id);
