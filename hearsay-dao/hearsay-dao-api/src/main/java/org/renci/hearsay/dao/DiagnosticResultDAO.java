3e12b184bb19c85fb5bff2d1966802fbb5955f7d
Jason Reilly <jdr0887@gmail.com>
2015-11-05 11:58:08 -0500

    cleaning

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/DiagnosticResultDAO.java
+++ hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/DiagnosticResultDAO.java
@@ -3,7 +3,6 @@ package org.renci.hearsay.dao;
 import java.util.List;
 
 import org.renci.hearsay.dao.model.DiagnosticResult;
-import org.renci.hearsay.dao.model.Gene;
 
 public interface DiagnosticResultDAO extends BaseEntityDAO<DiagnosticResult, Long> {
 
