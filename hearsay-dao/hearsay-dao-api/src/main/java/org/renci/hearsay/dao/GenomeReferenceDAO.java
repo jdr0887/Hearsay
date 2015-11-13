1769baab74c71a4e12f010ef6c73035f5e27500d
Jason Reilly <jdr0887@gmail.com>
2015-11-13 15:53:46 -0500

    adding findByName

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/GenomeReferenceDAO.java
+++ hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/GenomeReferenceDAO.java
@@ -8,6 +8,8 @@ public interface GenomeReferenceDAO extends BaseEntityDAO<GenomeReference, Long>
 
     public List<GenomeReference> findAll() throws HearsayDAOException;
 
+    public GenomeReference findByName(String name) throws HearsayDAOException;
+
     public List<GenomeReference> findByExample(GenomeReference genomeReference) throws HearsayDAOException;
 
     public List<GenomeReference> findByIdentifierValue(String value) throws HearsayDAOException;
