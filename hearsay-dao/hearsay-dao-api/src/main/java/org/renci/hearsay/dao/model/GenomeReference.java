e6f9b7bd2bb65bae9942f7df19a24ddd110acf76
Jason Reilly <jdr0887@gmail.com>
2016-01-08 10:32:24 -0500

    cleaning

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/model/GenomeReference.java
+++ hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/model/GenomeReference.java
@@ -17,7 +17,6 @@ import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.XmlType;
 
-import org.apache.openjpa.persistence.DataCache;
 import org.apache.openjpa.persistence.FetchAttribute;
 import org.apache.openjpa.persistence.FetchGroup;
 import org.apache.openjpa.persistence.FetchGroups;
@@ -34,7 +33,6 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 @Table(schema = "hearsay", name = "genome_reference", uniqueConstraints = { @UniqueConstraint(columnNames = "name") })
 @NamedQueries({ @NamedQuery(name = "GenomeReference.findAll", query = "SELECT a FROM GenomeReference a order by a.name"),
         @NamedQuery(name = "GenomeReference.findByName", query = "SELECT a FROM GenomeReference a where a.name = :name order by a.name") })
-@DataCache(timeout = 300000)
 @FetchGroups({ @FetchGroup(name = "includeAll", attributes = { @FetchAttribute(name = "referenceSequences") }) })
 public class GenomeReference extends IdentifiableEntity {
 
@@ -53,7 +51,7 @@ public class GenomeReference extends IdentifiableEntity {
     }
 
     public GenomeReference(String name) {
-        super();
+        this();
         this.name = name;
     }
 
