c3f78cee1ab7a14c3caac83c06910aaefa8aa5b1
Jason Reilly <jdr0887@gmail.com>
2016-01-08 10:40:35 -0500

    adding findById, adding hint to findByReferenceSequenceId

--- hearsay-dao/hearsay-dao-jpa/src/main/java/org/renci/hearsay/dao/jpa/AlignmentDAOImpl.java
+++ hearsay-dao/hearsay-dao-jpa/src/main/java/org/renci/hearsay/dao/jpa/AlignmentDAOImpl.java
@@ -12,6 +12,9 @@ import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import javax.transaction.Transactional;
 
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.openjpa.persistence.OpenJPAPersistence;
+import org.apache.openjpa.persistence.OpenJPAQuery;
 import org.ops4j.pax.cdi.api.OsgiServiceProvider;
 import org.renci.hearsay.dao.AlignmentDAO;
 import org.renci.hearsay.dao.HearsayDAOException;
@@ -49,18 +52,46 @@ public class AlignmentDAOImpl extends BaseEntityDAOImpl<Alignment, Long> impleme
     @Override
     public List<Alignment> findByReferenceSequenceId(Long referenceSequenceId) throws HearsayDAOException {
         logger.debug("ENTERING findByReferenceSequenceId(Long)");
-        CriteriaBuilder critBuilder = getEntityManager().getCriteriaBuilder();
-        CriteriaQuery<Alignment> crit = critBuilder.createQuery(getPersistentClass());
-        Root<Alignment> fromAlignment = crit.from(Alignment.class);
-        List<Predicate> predicates = new ArrayList<Predicate>();
-        Join<Alignment, ReferenceSequence> alignmentReferenceSequenceJoin = fromAlignment.join(Alignment_.referenceSequences);
-        predicates.add(critBuilder.equal(alignmentReferenceSequenceJoin.get(ReferenceSequence_.id), referenceSequenceId));
-        // predicates.add(critBuilder.isNotNull(fromAlignment.join(Alignment_.regions)));
-        // Join<Alignment, Region> alignmentRegionJoin = fromAlignment.join(Alignment_.regions);
-        // predicates.add(critBuilder.isNotNull(alignmentRegionJoin.get(Region_.id)));
-        crit.where(predicates.toArray(new Predicate[predicates.size()]));
-        TypedQuery<Alignment> query = getEntityManager().createQuery(crit);
-        List<Alignment> ret = query.getResultList();
+        List<Alignment> ret = new ArrayList<Alignment>();
+        try {
+            CriteriaBuilder critBuilder = getEntityManager().getCriteriaBuilder();
+            CriteriaQuery<Alignment> crit = critBuilder.createQuery(getPersistentClass());
+            Root<Alignment> fromAlignment = crit.from(Alignment.class);
+            List<Predicate> predicates = new ArrayList<Predicate>();
+            Join<Alignment, ReferenceSequence> alignmentReferenceSequenceJoin = fromAlignment.join(Alignment_.referenceSequences);
+            predicates.add(critBuilder.equal(alignmentReferenceSequenceJoin.get(ReferenceSequence_.id), referenceSequenceId));
+            crit.where(predicates.toArray(new Predicate[predicates.size()]));
+            TypedQuery<Alignment> query = getEntityManager().createQuery(crit);
+            OpenJPAQuery<Alignment> openjpaQuery = OpenJPAPersistence.cast(query);
+            openjpaQuery.getFetchPlan().addFetchGroup("includeRegions");
+            ret.addAll(openjpaQuery.getResultList());
+        } catch (Exception e) {
+            throw new HearsayDAOException(e);
+        }
+        return ret;
+    }
+
+    @Override
+    public Alignment findById(Long id) throws HearsayDAOException {
+        logger.debug("ENTERING findById(Long)");
+        Alignment ret = null;
+        try {
+            CriteriaBuilder critBuilder = getEntityManager().getCriteriaBuilder();
+            CriteriaQuery<Alignment> crit = critBuilder.createQuery(getPersistentClass());
+            Root<Alignment> fromAlignment = crit.from(Alignment.class);
+            List<Predicate> predicates = new ArrayList<Predicate>();
+            predicates.add(critBuilder.equal(fromAlignment.get(Alignment_.id), id));
+            crit.where(predicates.toArray(new Predicate[predicates.size()]));
+            TypedQuery<Alignment> query = getEntityManager().createQuery(crit);
+            OpenJPAQuery<Alignment> openjpaQuery = OpenJPAPersistence.cast(query);
+            openjpaQuery.getFetchPlan().addFetchGroup("includeRegions");
+            List<Alignment> results = openjpaQuery.getResultList();
+            if (CollectionUtils.isNotEmpty(results)) {
+                ret = results.get(0);
+            }
+        } catch (Exception e) {
+            throw new HearsayDAOException(e);
+        }
         return ret;
     }
 
