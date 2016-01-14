200c1b0e68eff079bb9d2c6712568a94879f6e7e
Jason Reilly <jdr0887@gmail.com>
2016-01-14 13:23:14 -0500

    adding findByAlignmentId

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/RegionDAO.java
+++ hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/RegionDAO.java
@@ -1,7 +1,11 @@
 package org.renci.hearsay.dao;
 
+import java.util.List;
+
 import org.renci.hearsay.dao.model.Region;
 
 public interface RegionDAO extends BaseEntityDAO<Region, Long> {
+
+    public List<Region> findByAlignmentId(Long alignmentId) throws HearsayDAOException;
 
 }
