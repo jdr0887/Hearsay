bea089fdd208763ece4ee7c5679d70de0a35730d
Jason Reilly <jdr0887@gmail.com>
2016-01-14 08:27:29 -0500

    exposing fetchplan arg

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/ReferenceSequenceDAO.java
+++ hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/ReferenceSequenceDAO.java
@@ -11,9 +11,13 @@ public interface ReferenceSequenceDAO extends BaseEntityDAO<ReferenceSequence, L
 
     public List<ReferenceSequence> findByExample(ReferenceSequence referenceSequence) throws HearsayDAOException;
 
-    public List<ReferenceSequence> findAll() throws HearsayDAOException;
+    public List<ReferenceSequence> findByIdentifierSystemAndValue(String system, String value) throws HearsayDAOException;
 
-    public List<ReferenceSequence> findByIdentifierValue(String value) throws HearsayDAOException;
+    public List<ReferenceSequence> findByIdentifierSystemAndValue(String fetchPlan, String system, String value) throws HearsayDAOException;
+
+    public List<ReferenceSequence> findByIdentifierSystem(String system) throws HearsayDAOException;
+
+    public List<ReferenceSequence> findByIdentifierSystem(String fetchPlan, String system) throws HearsayDAOException;
 
     public List<ReferenceSequence> findByIdentifiers(List<Identifier> idList) throws HearsayDAOException;
 
