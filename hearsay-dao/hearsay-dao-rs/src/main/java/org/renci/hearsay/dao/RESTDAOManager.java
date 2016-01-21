f07d798d330370d7ac2b0c442f2359d1bb1e0a34
Jason Reilly <jdr0887@gmail.com>
2016-01-21 13:54:48 -0500

    using newer spring

---
+++ hearsay-dao/hearsay-dao-rs/src/main/java/org/renci/hearsay/dao/RESTDAOManager.java
@@ -0,0 +1,38 @@
+package org.renci.hearsay.dao;
+
+import org.renci.hearsay.dao.HearsayDAOBeanService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * 
+ * @author jdr0887
+ */
+public class RESTDAOManager {
+
+    private static RESTDAOManager instance;
+
+    private HearsayDAOBeanService hearsayDAOBeanService;
+
+    public static RESTDAOManager getInstance() {
+        if (instance == null) {
+            instance = new RESTDAOManager();
+        }
+        return instance;
+    }
+
+    private RESTDAOManager() {
+        super();
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+                "classpath:/org/renci/hearsay/dao/rs/hearsay-dao-beans.xml");
+        this.hearsayDAOBeanService = applicationContext.getBean(HearsayDAOBeanService.class);
+    }
+
+    public HearsayDAOBeanService getHearsayDAOBeanService() {
+        return hearsayDAOBeanService;
+    }
+
+    public void setHearsayDAOBeanService(HearsayDAOBeanService hearsayDAOBeanService) {
+        this.hearsayDAOBeanService = hearsayDAOBeanService;
+    }
+
+}
