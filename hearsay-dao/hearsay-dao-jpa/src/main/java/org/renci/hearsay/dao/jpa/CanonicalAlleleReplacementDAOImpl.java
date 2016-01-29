21e660a4bd9971649ea6981ad1cfab8259a145bf
Jason Reilly <jdr0887@gmail.com>
2016-01-29 08:57:26 -0500

    formatting

--- hearsay-dao/hearsay-dao-jpa/src/main/java/org/renci/hearsay/dao/jpa/CanonicalAlleleReplacementDAOImpl.java
+++ hearsay-dao/hearsay-dao-jpa/src/main/java/org/renci/hearsay/dao/jpa/CanonicalAlleleReplacementDAOImpl.java
@@ -4,15 +4,14 @@ import javax.inject.Singleton;
 import javax.transaction.Transactional;
 
 import org.ops4j.pax.cdi.api.OsgiServiceProvider;
-import org.renci.hearsay.dao.CanonicalAlleleDAO;
 import org.renci.hearsay.dao.CanonicalAlleleReplacementDAO;
-import org.renci.hearsay.dao.model.CanonicalAllele;
 import org.renci.hearsay.dao.model.CanonicalAlleleReplacement;
 
 @OsgiServiceProvider(classes = { CanonicalAlleleReplacementDAO.class })
 @Singleton
 @Transactional(Transactional.TxType.SUPPORTS)
-public class CanonicalAlleleReplacementDAOImpl extends BaseEntityDAOImpl<CanonicalAlleleReplacement, Long> implements CanonicalAlleleReplacementDAO {
+public class CanonicalAlleleReplacementDAOImpl extends BaseEntityDAOImpl<CanonicalAlleleReplacement, Long>
+        implements CanonicalAlleleReplacementDAO {
 
     public CanonicalAlleleReplacementDAOImpl() {
         super();
