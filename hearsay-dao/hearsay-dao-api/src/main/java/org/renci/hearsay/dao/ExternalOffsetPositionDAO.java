eaa1620f69df54eaf2e2f81f3b4d1e47cc82b9aa
Jason Reilly <jdr0887@gmail.com>
2016-02-12 11:16:39 -0500

    renamed

---
+++ hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/ExternalOffsetPositionDAO.java
@@ -0,0 +1,7 @@
+package org.renci.hearsay.dao;
+
+import org.renci.hearsay.dao.model.ExternalOffsetPosition;
+
+public interface ExternalOffsetPositionDAO extends BaseEntityDAO<ExternalOffsetPosition, Long> {
+
+}
