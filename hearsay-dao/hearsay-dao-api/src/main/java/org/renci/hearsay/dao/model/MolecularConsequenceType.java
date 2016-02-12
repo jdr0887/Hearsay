f7518ed6791260875015a78961f4b0dad930156b
Jason Reilly <jdr0887@gmail.com>
2016-02-12 11:18:33 -0500

    not sure this is right

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/model/MolecularConsequenceType.java
+++ hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/model/MolecularConsequenceType.java
@@ -2,12 +2,54 @@ package org.renci.hearsay.dao.model;
 
 public enum MolecularConsequenceType {
 
-    PRIMARY_NUCLEOTIDE,
+    TWO_THOUSAND_B_UPSTREAM_VARIANT("2KB upstream variant"),
 
-    PRIMARY_AMINO_ACID,
+    INTERGENIC_VARIANT("intergenic_variant"),
 
-    ANCILLARY_NUCLEOTIDE,
+    SPLICE_SITE("Splice Site"),
 
-    ANCILLARY_AMINO_ACID;
+    FRAMESHIFT_VARIANT("frameshift variant"),
+
+    SYNONYMOUS_VARIANT("synonymous variant"),
+
+    STOP_LOST("stop lost"),
+
+    MISSENSE_VARIANT("missense variant"),
+
+    NONSENSE("nonsense"),
+
+    NON_CODING_TRANSCRIPT_VARIANT("non-coding transcript variant"),
+
+    FIVE_HUNDRED_B_DOWNSTREAM_VARIANT("500B downstream variant"),
+
+    SPLICE_DONOR_VARIANT("splice donor variant"),
+
+    EXON_LOSS("exon_loss"),
+
+    THREE_PRIME_UTR_VARIANT("3 prime UTR variant"),
+
+    SPLICE_ACCEPTOR_VARIANT("splice acceptor variant"),
+
+    INTRON_VARIANT("intron variant"),
+
+    INFRAME_VARIANT("inframe_variant"),
+
+    FIVE_PRIME_UTR_VARIANT("5 prime UTR variant"),
+
+    REGULAR_REGION_ABLATION("regulatory region ablation");
+
+    private String value;
+
+    private MolecularConsequenceType(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 
 }
