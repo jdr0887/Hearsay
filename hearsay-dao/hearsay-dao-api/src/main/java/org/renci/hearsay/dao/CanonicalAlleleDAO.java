ea4b1e34967200b9cb7093f52c46369f13cd6b41
Jason Reilly <jdr0887@gmail.com>
2016-02-12 11:16:28 -0500

    adding findByIdentifierSystemAndValue

--- hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/CanonicalAlleleDAO.java
+++ hearsay-dao/hearsay-dao-api/src/main/java/org/renci/hearsay/dao/CanonicalAlleleDAO.java
@@ -1,7 +1,13 @@
 package org.renci.hearsay.dao;
 
+import java.util.List;
+
 import org.renci.hearsay.dao.model.CanonicalAllele;
 
 public interface CanonicalAlleleDAO extends BaseEntityDAO<CanonicalAllele, Long> {
+
+    public List<CanonicalAllele> findByIdentifierSystemAndValue(String system, String value) throws HearsayDAOException;
+
+    public List<CanonicalAllele> findByIdentifierSystemAndValue(String fetchPlan, String system, String value) throws HearsayDAOException;
 
 }
